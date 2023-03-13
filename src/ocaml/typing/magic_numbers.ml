2b5564b96e3ae6748b318eff4d066a493c09bcc2
Daniel Dickstein <ddickstein@janestreet.com>
2023-03-13 19:14:11 -0400

    Revert "Revert "Make Merlin error messages robust to missing Merlin version""
    This reverts commit b0f4cebc9bdcd23aa08cf85868293815c87e895b.

--- src/ocaml/typing/magic_numbers.ml
+++ src/ocaml/typing/magic_numbers.ml
@@ -32,22 +32,30 @@ module Cmi = struct
         fprintf ppf "%a@ is not a compiled interface"
           Location.print_filename filename
     | Wrong_version_interface (filename, compiler_magic) ->
+      let merlin_ocaml_version =
+        let ocaml_version =
+          match to_version_opt Config.cmi_magic_number with
+          | Some version -> "OCaml " ^ version
+          | None -> "an unknown version of OCaml"
+        in
+        sprintf "%s (with magic number %s)" ocaml_version Config.cmi_magic_number
+      in
       begin match to_version_opt compiler_magic with
       | None ->
         fprintf ppf
           "%a@ seems to be compiled with a version of OCaml (with magic number \
            %s) that is not supported by Merlin.@.\
-          This instance of Merlin handles OCaml %s."
+          This instance of Merlin handles %s."
           Location.print_filename filename
           compiler_magic
-          (Option.get @@ to_version_opt Config.cmi_magic_number)
+          merlin_ocaml_version
       | Some version ->
         fprintf ppf
           "%a@ seems to be compiled with OCaml %s.@.\
            But this instance of Merlin handles OCaml %s."
           Location.print_filename filename
           version
-          (Option.get @@ to_version_opt Config.cmi_magic_number)
+          merlin_ocaml_version
       end
     | Corrupted_interface filename ->
       fprintf ppf
