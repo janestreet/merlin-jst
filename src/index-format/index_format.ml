2fa4bbdd3113300ec5cf079ce71a7e1776249f45
Pierre Oechsel <pierre.oechsel@gmail.com>
2024-02-26 15:05:38 +0000

    fix bad rebase of index_format

--- src/index-format/index_format.ml
+++ src/index-format/index_format.ml
@@ -10,7 +10,6 @@ module Lid : Set.OrderedType with type t = Longident.t Location.loc = struct
     | n -> n
 
   let compare (t1 : t) (t2 : t) =
-    (* TODO CHECK...*)
     match compare_pos t1.loc.loc_start t2.loc.loc_start with
     | 0 -> compare_pos t1.loc.loc_end t2.loc.loc_end
     | n -> n
@@ -21,109 +20,28 @@ module Stats = Map.Make (String)
 
 (** [add tbl uid locs] adds a binding of [uid] to the locations [locs]. If this key is
     already present the locations are merged. *)
-    let add tbl uid locs =
-      try
-        let locations = Hashtbl.find tbl uid in
-        Hashtbl.replace tbl uid (LidSet.union locs locations)
-      with Not_found -> Hashtbl.add tbl uid locs
+let add tbl uid locs =
+  try
+    let locations = Hashtbl.find tbl uid in
+    Hashtbl.replace tbl uid (LidSet.union locs locations)
+  with Not_found -> Hashtbl.add tbl uid locs
 
-    type stat = { mtime : float; size : int; source_digest: string option }
-    type index = {
-      defs : (Shape.Uid.t, LidSet.t) Hashtbl.t;
-      approximated : (Shape.Uid.t, LidSet.t) Hashtbl.t;
-      load_path : Load_path.paths;
-      cu_shape : (string, Shape.t) Hashtbl.t;
-      stats : stat Stats.t;
-    }
-
-    let pp_partials (fmt : Format.formatter)
-        (partials : (Shape.Uid.t, LidSet.t) Hashtbl.t) =
-      Format.fprintf fmt "{@[";
-      Hashtbl.iter
-        (fun uid locs ->
-          Format.fprintf fmt "@[<hov 2>uid: %a; locs:@ @[<v>%a@]@]@;"
-            Shape.Uid.print uid
-            (Format.pp_print_list
-               ~pp_sep:(fun fmt () -> Format.fprintf fmt ";@;")
-               (fun fmt { Location.txt; loc } ->
-                 Format.fprintf fmt "%S: %a"
-                   (try Longident.flatten txt |> String.concat "." with _ -> "<?>")
-                   Location.print_loc loc))
-            (LidSet.elements locs))
-        partials;
-      Format.fprintf fmt "@]}"
-
-    let pp (fmt : Format.formatter) pl =
-      Format.fprintf fmt "%i uids:@ {@[" (Hashtbl.length pl.defs);
-      Hashtbl.iter
-        (fun uid locs ->
-          Format.fprintf fmt "@[<hov 2>uid: %a; locs:@ @[<v>%a@]@]@;"
-            Shape.Uid.print uid
-            (Format.pp_print_list
-               ~pp_sep:(fun fmt () -> Format.fprintf fmt ";@;")
-               (fun fmt { Location.txt; loc } ->
-                 Format.fprintf fmt "%S: %a"
-                   (try Longident.flatten txt |> String.concat "." with _ -> "<?>")
-                   Location.print_loc loc))
-            (LidSet.elements locs))
-        pl.defs;
-      Format.fprintf fmt "@]},@ ";
-      Format.fprintf fmt "%i approx shapes:@ @[%a@],@ "
-        (Hashtbl.length pl.approximated)
-        pp_partials pl.approximated;
-      Format.fprintf fmt "and shapes for CUS %s.@ "
-        (String.concat ";@," (Hashtbl.to_seq_keys pl.cu_shape |> List.of_seq))
-
-    let ext = "ocaml-index"
-
-    (* [magic_number] Must be the same lenght as cmt's magic numbers *)
-    let magic_number = "Merl2023I001"
-
-    let write ~file index =
-      Misc.output_to_file_via_temporary ~mode:[ Open_binary ] file
-        (fun _temp_file_name oc ->
-          output_string oc magic_number;
-          output_value oc (index : index))
-
-    type file_content = Cmt of Cmt_format.cmt_infos | Index of index | Unknown
-
-    let read ~file =
-      let ic = open_in_bin file in
-      Merlin_utils.Misc.try_finally
-        ~always:(fun () -> close_in ic)
-        (fun () ->
-          let file_magic_number = ref (Cmt_format.read_magic_number ic) in
-          let cmi_magic_number = Ocaml_utils.Config.cmi_magic_number in
-          let cmt_magic_number = Ocaml_utils.Config.cmt_magic_number in
-          (if String.equal !file_magic_number cmi_magic_number then
-             let _ = Cmi_format.input_cmi ic in
-             file_magic_number := Cmt_format.read_magic_number ic);
-          if String.equal !file_magic_number cmt_magic_number then
-            Cmt (input_value ic : Cmt_format.cmt_infos)
-          else if String.equal !file_magic_number magic_number then
-            Index (input_value ic : index)
-          else Unknown)
-
-    let read_exn ~file =
-      match read ~file with Index index -> index | _ -> raise (Not_an_index file)
-
-type payload = {
+type stat = { mtime : float; size : int; source_digest: string option }
+type index = {
   defs : (Shape.Uid.t, LidSet.t) Hashtbl.t;
-  partials : (Shape.t, LidSet.t) Hashtbl.t;
-  unreduced : (Shape.t * Longident.t Location.loc) list;
-  load_path : string list;
+  approximated : (Shape.Uid.t, LidSet.t) Hashtbl.t;
+  load_path : Load_path.paths;
   cu_shape : (string, Shape.t) Hashtbl.t;
+  stats : stat Stats.t;
 }
 
-type file_format = V1 of payload
-
 let pp_partials (fmt : Format.formatter)
-    (partials : (Shape.t, LidSet.t) Hashtbl.t) =
+    (partials : (Shape.Uid.t, LidSet.t) Hashtbl.t) =
   Format.fprintf fmt "{@[";
   Hashtbl.iter
-    (fun shape locs ->
-      Format.fprintf fmt "@[<hov 2>shape: %a; locs:@ @[<v>%a@]@]@;" Shape.print
-        shape
+    (fun uid locs ->
+      Format.fprintf fmt "@[<hov 2>uid: %a; locs:@ @[<v>%a@]@]@;"
+        Shape.Uid.print uid
         (Format.pp_print_list
            ~pp_sep:(fun fmt () -> Format.fprintf fmt ";@;")
            (fun fmt { Location.txt; loc } ->
@@ -134,19 +52,7 @@ let pp_partials (fmt : Format.formatter)
     partials;
   Format.fprintf fmt "@]}"
 
-let pp_unreduced (fmt : Format.formatter)
-    (unreduced : (Shape.t * Longident.t Location.loc) list) =
-  Format.fprintf fmt "{@[";
-  List.iter
-    (fun (shape, { Location.txt; loc }) ->
-      Format.fprintf fmt "@[<hov 2>shape: %a; locs:@ @[<v>%s: %a@]@]@;"
-        Shape.print shape
-        (try Longident.flatten txt |> String.concat "." with _ -> "<?>")
-        Location.print_loc loc)
-    unreduced;
-  Format.fprintf fmt "@]}"
-
-let pp_payload (fmt : Format.formatter) pl =
+let pp (fmt : Format.formatter) pl =
   Format.fprintf fmt "%i uids:@ {@[" (Hashtbl.length pl.defs);
   Hashtbl.iter
     (fun uid locs ->
@@ -161,31 +67,41 @@ let pp_payload (fmt : Format.formatter) pl =
         (LidSet.elements locs))
     pl.defs;
   Format.fprintf fmt "@]},@ ";
-  Format.fprintf fmt "%i partial shapes:@ @[%a@],@ "
-    (Hashtbl.length pl.partials)
-    pp_partials pl.partials;
-  Format.fprintf fmt "%i unreduced shapes:@ @[%a@]@ " (List.length pl.unreduced)
-    pp_unreduced pl.unreduced;
+  Format.fprintf fmt "%i approx shapes:@ @[%a@],@ "
+    (Hashtbl.length pl.approximated)
+    pp_partials pl.approximated;
   Format.fprintf fmt "and shapes for CUS %s.@ "
     (String.concat ";@," (Hashtbl.to_seq_keys pl.cu_shape |> List.of_seq))
 
-let _pp (fmt : Format.formatter) ff =
-  match ff with V1 tbl -> Format.fprintf fmt "V1@,%a" pp_payload tbl
+let ext = "ocaml-index"
 
-let _ext = "uideps"
+(* [magic_number] Must be the same lenght as cmt's magic numbers *)
+let magic_number = "Merl2023I001"
 
-let _write ~file tbl =
-  let oc = open_out_bin file in
-  Marshal.to_channel oc (V1 tbl) [];
-  close_out oc
+let write ~file index =
+  Misc.output_to_file_via_temporary ~mode:[ Open_binary ] file
+    (fun _temp_file_name oc ->
+      output_string oc magic_number;
+      output_value oc (index : index))
+
+type file_content = Cmt of Cmt_format.cmt_infos | Index of index | Unknown
 
 let read ~file =
   let ic = open_in_bin file in
-  try
-    let payload =
-      match Marshal.from_channel ic with V1 payload -> payload
-      (* TODO is that "safe" ? We probably want some magic number *)
-    in
-    close_in ic;
-    payload
-  with e -> raise e (* todo *)
+  Merlin_utils.Misc.try_finally
+    ~always:(fun () -> close_in ic)
+    (fun () ->
+      let file_magic_number = ref (Cmt_format.read_magic_number ic) in
+      let cmi_magic_number = Ocaml_utils.Config.cmi_magic_number in
+      let cmt_magic_number = Ocaml_utils.Config.cmt_magic_number in
+      (if String.equal !file_magic_number cmi_magic_number then
+         let _ = Cmi_format.input_cmi ic in
+         file_magic_number := Cmt_format.read_magic_number ic);
+      if String.equal !file_magic_number cmt_magic_number then
+        Cmt (input_value ic : Cmt_format.cmt_infos)
+      else if String.equal !file_magic_number magic_number then
+        Index (input_value ic : index)
+      else Unknown)
+
+let read_exn ~file =
+  match read ~file with Index index -> index | _ -> raise (Not_an_index file)
