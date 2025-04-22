ab93d4cc175e182f8091b9fee99b9faea308aa17
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-04-22 15:35:50 -0400

    fix
    Signed-off-by: Patrick Ferris <patrick@sirref.org>

--- src/ocaml/parsing/msupport_parsing.ml
+++ src/ocaml/parsing/msupport_parsing.ml
@@ -1,4 +1,4 @@
 (* Filled in from Msupport. *)
-let msupport_raise_error : (exn -> unit) ref = ref raise
+let msupport_raise_error : (exn -> unit) ref = ref (fun x -> raise x)
 
 let raise_error exn = !msupport_raise_error exn
